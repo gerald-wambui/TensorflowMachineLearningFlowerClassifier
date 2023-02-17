500fa9f554f404e53eeb2ec5a9efbc6b815236a5
Gerald <geraldwainaina25@gmail.com>
2023-02-17 14:38:47 +0300

    Update flowerclassifier.py

--- flowerclassifier.py
+++ flowerclassifier.py
@@ -17,4 +17,36 @@ base_dir = os.path.join(os.path.dirname(zip_file), 'flower_photos')
 #creating labels
 classes = ['roses', 'daisy', 'dandelion', 'sunflowers', 'tulips']
 
-#for cl in classes:
+#for cl in classes
+for cl in classes:
+  img_path = os.path.join(base_dir, cl)
+  images = glob.glob(img_path + '/*.jpg')
+  print("{}: {} Images".format(cl, len(images)))
+  train, val = images[:round(len(images)*0.8)], images[round(len(images)*0.8):]
+
+  for t in train:
+    if not os.path.exists(os.path.join(base_dir, 'train', cl)):
+      os.makedirs(os.path.join(base_dir, 'train', cl))
+    shutil.move(t, os.path.join(base_dir, 'train', cl))
+
+  for v in val:
+    if not os.path.exists(os.path.join(base_dir, 'val', cl)):
+      os.makedirs(os.path.join(base_dir, 'val', cl))
+    shutil.move(v, os.path.join(base_dir, 'val', cl))
+
+train_dir = os.path.join(base_dir, 'train')
+val_dir = os.path.join(base_dir, 'val')
+
+#data augmentation
+
+batch_size = 100
+IMG_SHAPE = 150
+
+#random horizontal flip
+image_gen = ImageDataGenerator(rescale=1./255, horizontal_flip=True)
+train_data_gen = image_gen.flow_from_directory(batch_size=batch_size,
+                                               directory=train_dir,
+                                               shuffle=True,
+                                               target_size=(IMG_SHAPE, IMG_SHAPE))
+
+#funto plot images
