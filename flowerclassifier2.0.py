0e65947c01cd5b1461fe6589083418c1060100d0
Gerald <geraldwainaina25@gmail.com>
2023-02-22 07:09:34 +0300

    Update flowerclassifier2.0.py

--- flowerclassifier2.0.py
+++ flowerclassifier2.0.py
@@ -64,3 +64,20 @@ def plotImages(images_arr):
 
 augmented_images = [train_data_gen[0][0][0] for i in range(5)]
 plotImages(augmented_images)
+
+image_gen = ImageDataGenerator(rescale=1./255, rotation_range=45)
+train_data_gen = image_gen.flow_from_directory(batch_size=batch_size,
+                                               directory=train_dir,
+                                               shuffle=True,
+                                               target_size=(IMG_SHAPE, IMG_SHAPE)
+                                               )
+def plotImages(images_arr):
+  fig, axes = plt.subplots(1, 5, figsize=(20, 20))
+  axes = axes.flatten()
+  for img, ax in zip(images_arr, axes):
+    ax.imshow(img)
+  plt.tight_layout()
+  plt.show()
+
+augmented_images = [train_data_gen[0][0][0] for i in range(5)]
+plotImages(augmented_images)
